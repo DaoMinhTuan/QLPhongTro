52aa0b149f3d75fc3245db40a175cba80bc94ceb
daoTuan <minhtuan080722@gmail.com>
2022-10-25 16:06:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/activity/DoanhThuThangActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+import com.example.qlphongtro.R;
+
+public class DoanhThuThangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doanh_thu_thang);
+    }
+}
